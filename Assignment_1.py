0f9dbbce044d5fc526e5a1e8879ff734abfe2b73
PolarisZhang <60202791+PolarisZhang@users.noreply.github.com>
2020-02-01 12:00:14 -0500

    Update Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -3,15 +3,13 @@ import matplotlib.pyplot as plt
 
 
 def graph():
-    x = np.arange(0,2*np.pi,0.1)  ## one period of sine & cosine function is 2pi, 
-                                  ## start from 0, end at 2pi, stpes in 0.1
+    x = np.arange(0,2*np.pi,0.1)  ## one period of sine & cosine function is 2pi, start from 0, end at 2pi, stpes in 0.1
     y = np.sin(x)
     z = np.cos(x)
     plt.plot(x, y, x, z)
     plt.legend(['sin(x)', 'cos(x)'])
     plt.xlabel('x value from 0 to 2pi')
 
-    
     plt.ylim(-2, 2)
     plt.legend(['sin(x)', 'cos(x)'])
     plt.show()
